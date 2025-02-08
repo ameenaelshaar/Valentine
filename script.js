57507f363a3c2ec77686daece5249940f8d91daa
ameenaelshaar <elshaarameena@gmail.com>
2025-02-07 22:13:49 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.getElementById("loveButton");
-    const message = document.getElementById("message");
-
-    button.addEventListener("click", function () {
-        message.classList.toggle("hidden");
-    });
-});
